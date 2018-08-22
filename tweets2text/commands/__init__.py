b69e3a585affec95e097dcfd65dec2b50708b8dd
James Gordon <gordon.je@gmail.com>
2018-08-21 21:53:25 -0500

    close #21

--- tweets2text/commands/__init__.py
+++ tweets2text/commands/__init__.py
@@ -4,7 +4,7 @@
 Custom commands for managing an instance of the tweets2text app.
 """
 from .dynamodb import dynamodb_cli 
-# from .s3 import s3_cli
+from .s3 import s3_cli
 from .twitter import twitter_cli
 
 
@@ -12,7 +12,6 @@ def add_all(app):
     """
     Add all custom commands to an app instance.
     """
-    # app.cli.add_command(dynamodb.create_all_tables_command)
     app.cli.add_command(dynamodb_cli)
-    # app.cli.add_command(s3_cli)
+    app.cli.add_command(s3_cli)
     app.cli.add_command(twitter_cli)
--- tweets2text/commands/s3.py
+++ tweets2text/commands/s3.py
@@ -1,7 +1,27 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Custom commands for managing tweet2text's integration with AWS DynamoDB.
+Custom commands for managing tweet2text's integration with AWS S3.
 """
+import click
+from flask.cli import AppGroup, with_appcontext
+from tweets2text.s3 import get_bucket
 
 
+s3_cli = AppGroup('s3')
+
+
+@s3_cli.command('create-bucket')
+@with_appcontext
+def create_bucket_command():
+    """
+    Create the S3 bucket specified in the `S3_BUCKET_NAME` env var.
+    """
+    bucket = get_bucket()
+    
+    if not bucket.creation_date:
+        bucket.create()
+        bucket.meta.client.get_waiter('bucket_exists')
+        click.echo('%s created.' % bucket.name)
+    else:
+        click.echo('%s already exists.' % bucket.name)
