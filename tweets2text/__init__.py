567c9748feb01c59f27ec18e10a2e528c66077a8
James Gordon <gordon.je@gmail.com>
2018-10-26 14:00:45 -0500

    Set the testing config

--- tweets2text/__init__.py
+++ tweets2text/__init__.py
@@ -15,6 +15,13 @@ def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
 
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
     from .commands import add_all as add_all_commands
     add_all_commands(app)
 
