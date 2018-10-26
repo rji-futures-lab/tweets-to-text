c076e9785daf296b3468e7e8660e31ac2dd2eb85
James Gordon <gordon.je@gmail.com>
2018-10-26 14:02:35 -0500

    Whip unittest style tests into shape.

--- tests/test_tweets2text.py
+++ tests/test_tweets2text.py
@@ -1,13 +1,29 @@
 """
 Unit tests.
 """
+from contextlib import contextmanager
 import unittest
-from flask import current_app, g
-from moto import mock_dynamodb
+from flask import appcontext_pushed, g
+import boto3
 import requests_mock
 from tweets2text import create_app
-from tweets2text.dynamodb import get_dynamodb
-# from tweets2text.handlers.twitter import handle_account_activity
+from tweets2text.dynamodb import get_dynamodb, schema
+
+
+@contextmanager
+def dynamodb_set(app, dynamodb_resource):
+    def handler(sender, **kwargs):
+        g.dynamodb = dynamodb_resource
+    with appcontext_pushed.connected_to(handler, app):
+        yield
+
+
+@contextmanager
+def s3_set(app, s3_resource):
+    def handler(sender, **kwargs):
+        g.s3 = s3_resource
+    with appcontext_pushed.connected_to(handler, app):
+        yield
 
 
 incoming_follow = {
@@ -29,6 +45,25 @@ incoming_follow = {
 }
 
 
+outgoing_follow = {
+        "for_user_id": "1017142357932769280",
+        "follow_events": [
+            {
+                "type": "follow",
+                "created_timestamp": "1535652417571",
+                "target": {
+                    "id": "1029178631921303553",
+                    "name": "Bot2BotAction",
+                },
+                "source": {
+                    "id": "1017142357932769280",
+                    "name": "TweetToText",
+                }
+            }
+        ]
+    }
+
+
 class TestTweetsToTextBase(unittest.TestCase):
     """
     Base class for all TweetsToText test cases.
@@ -42,8 +77,6 @@ class TestTweetsToTextBase(unittest.TestCase):
             'TESTING': True,
         })
 
-        cls.client = cls.app.test_client()
-
 
 class TestIndexView(TestTweetsToTextBase):
     """
@@ -53,59 +86,129 @@ class TestIndexView(TestTweetsToTextBase):
         """
         Confirm the index responds with OK status.
         """
-        response = self.client.get('/')
+        with self.app.test_client() as c:
+            response = c.get('/')
 
         self.assertEqual(response.status_code, 200)
 
 
-class TestIncomingFollow(TestTweetsToTextBase):
+class TestDynamoDbBase(TestTweetsToTextBase):
+    """
+    Base class for all TweetsToText test cases involving DynamoDb
+    """
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up the test case.
+        """
+        super(TestDynamoDbBase, cls).setUpClass()
+        
+        cls.dynamodb_local = boto3.resource(
+            'dynamodb', endpoint_url='http://localhost:8000'
+        )
+        # start the local DynamoDb instance
+        # java -Djava.library.path=./DynamoDBLocal_lib -jar /tmp/dynamodb/DynamoDBLocal.jar -sharedDb
+        for table_def in schema:
+            try:
+                cls.dynamodb_local.create_table(**table_def)
+            except:
+                pass
+
+    @classmethod
+    def tearDownClass(cls):
+        for table_def in schema:
+            table_name = table_def['TableName']
+            cls.dynamodb_local.Table(table_name).delete()
+
+
+
+class TestIncomingFollow(TestDynamoDbBase):
     """
     Test case for incoming follow account activity event.
     """
     @classmethod
-    @mock_dynamodb
     @requests_mock.Mocker()
     def setUpClass(cls, m):
         """
         Set up the test case.
         """
         super(TestIncomingFollow, cls).setUpClass()
-
-        m.register_uri(
+        
+        cls.m = m.register_uri(
             'POST', 
             'https://api.twitter.com/1.1/friendships/create.json',
             json=dict()
         )
 
-        with cls.app.app_context():
-            cls.dynamodb = get_dynamodb()
-            cls.app.logger.info('inside with app_context()')
-            cls.app.logger.info('cls.dynamodb')
-            cls.app.logger.info(id(cls.dynamodb))
-            cls.app.logger.info('g.dynamodb')
-            cls.app.logger.info(id(g.dynamodb))
-
-            cls.response = cls.client.post(
-                '/webhooks/twitter/',
-                json=incoming_follow
-            )
+        with dynamodb_set(cls.app, cls.dynamodb_local):
+            with cls.app.test_client() as c:
+                cls.response = c.post(
+                    '/webhooks/twitter/',
+                    json=incoming_follow
+                )
 
     def test_saved(self):
         """
         Confirm that the account activity event was saved.
-        """
-        self.app.logger.info('inside test_saved()')
-        self.app.logger.info('self.dynamodb')
-        self.app.logger.info(id(self.dynamodb))
-
-        table = self.dynamodb.Table(
+        """        
+        table_scan = self.dynamodb_local.Table(
             'TweetsToText-account-activity'
+        ).scan(Select='COUNT')
+
+        self.assertEqual(table_scan['Count'], 1)
+
+    def test_calls_to_twitter(self):
+        """
+        Confirm that Twitter was called only once.
+        """
+        self.assertEqual(self.m.call_count, 1)
+
+    def test_new_followers_count(self):
+        """
+        Confirm the new_followers count in response is correct.
+        """
+        count_new_followers = self.response.json['new_followers']
+        self.assertEqual(count_new_followers, 1)
+
+
+class TestOutgoingFollow(TestDynamoDbBase):
+    """
+    Test case for outgoing follow account activity event.
+    """
+    @classmethod
+    @requests_mock.Mocker()
+    def setUpClass(cls, m):
+        """
+        Set up the test case.
+        """
+        super(TestOutgoingFollow, cls).setUpClass()
+        
+        cls.m = m.register_uri(
+            'POST', 
+            'https://api.twitter.com/1.1/friendships/create.json',
+            json=dict()
         )
 
-        scan = table.scan(Select='COUNT')
+        with dynamodb_set(cls.app, cls.dynamodb_local):
+            with cls.app.test_client() as c:
+                cls.response = c.post(
+                    '/webhooks/twitter/',
+                    json=outgoing_follow
+                )
 
-        self.assertEqual(scan['Count'], 1)
+    def test_calls_to_twitter(self):
+        """
+        Confirm that Twitter was called only once.
+        """
+        self.assertEqual(self.m.call_count, 0)
 
-# class TestOutgoingFollow
+    def test_new_followers_count(self):
+        """
+        Confirm the new_followers count in response is correct.
+        """
+        count_new_followers = self.response.json['new_followers']
+        self.assertEqual(count_new_followers, 0)
+
+
 # class TestInitialMention
 # class TestFinalMention
